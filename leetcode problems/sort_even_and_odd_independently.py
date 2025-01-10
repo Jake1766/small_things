2a8f8faf72dace4c2affc7efeadb5317044acef1
Jake <jakerandall1766@gmail.com>
2025-01-10 17:47:19 +0000

    leetcode problem

--- leetcode problems/sort_even_and_odd_independently.py
+++ leetcode problems/sort_even_and_odd_independently.py
@@ -5,3 +5,25 @@
 # non increasing order means that each element is no larger than any
 # previous elements
 
+nums1 = [4, 1, 2, 3]
+
+def sort(arr):
+    odds = []
+    evens = []
+
+    for ind in range(len(arr)):
+        if ind % 2 == 0:
+            evens.append(arr[ind])
+        else:
+            odds.append(arr[ind])
+
+    odds = sorted(odds, reverse=True)
+    evens = sorted(evens)
+
+    print(odds)
+    print(evens)
+
+    for i in range(len(arr)):
+
+
+sort(nums1)
