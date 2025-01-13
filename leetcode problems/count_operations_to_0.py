a37bd115fc6c84b794e0749936af582fac0bc3d9
Jake <jakerandall1766@gmail.com>
2025-01-13 13:55:10 +0000

    working problem

---
+++ leetcode problems/count_operations_to_0.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/count-operations-to-obtain-zero/description/
+
+# num1 and num2 are provided
+# subtract smaller num from larger num
+# repeat until 0 is reached
+# return number of cycles taken to reach 0
+
+num1 = 2
+num2 = 3
+
+# output should be 3
+
+def count_0(num1, num2):
+    pass
