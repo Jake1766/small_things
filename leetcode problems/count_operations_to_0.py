7166ab687be8f4670415817e394ed27acf750305
Jake <jakerandall1766@gmail.com>
2025-01-18 12:34:14 +0000

    leetcode

--- leetcode problems/count_operations_to_0.py
+++ leetcode problems/count_operations_to_0.py
@@ -5,29 +5,44 @@
 # repeat until 0 is reached
 # return number of cycles taken to reach 0
 
-num1 = 99999
-num2 = 1
+#possible solution involves division
+
+# fail case 137/161 passed:
+# num1 = 0, num2 = 1
+
+# fail case 148/167 passed
+# num1 = 99999 num2 = 1
+
+
+num1 = 2
+num2 = 3
+
 
 # output should be 0
 
 def count_0(num1, num2):
     zero = False
     count = 0
-    if num1 == 0 and num2 == 0:
-        return 0
+    if num1 == 0 or num2 == 0:
+        return count
 
     while not zero:
         if num1 <= num2:
             num2 = num2 - num1
         else:
             num1 = num1 - num2
+        #conditional essential to break loop
+        if num1 == 0 or num2 == 0:
+            zero = True
 
         print("num1: ", num1, "\nnum2: ", num2)
         count += 1
 
-        if num1 == 0 or num2 == 0:
-            zero = True
+
     print("count = ", count)
     return count
 
-count_0(num1, num2)
+def count2_0(num1, num2):
+    print(num2/num1)
+
+print(count_0(num1, num2))
