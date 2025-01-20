b49247452380e67c6f7d0ff6b5a84ceac5f67367
Jake <jakerandall1766@gmail.com>
2025-01-20 10:45:27 +0000

    leetcode

---
+++ leetcode problems/minimum_operations_to_make_array_alternating.py
@@ -0,0 +1,4 @@
+# https://leetcode.com/problems/minimum-operations-to-make-the-array-alternating/
+
+# given an array such as nums = [3, 1, 3, 2, 4, 3] find the minimum number
+# of operations to make the array alternatinf, i.e [3, 1, 3, 1, 3, 1]
